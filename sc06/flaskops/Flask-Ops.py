77da775e6e418ed502635bb7795b89c1277f53d6
reagul <reagul.2007@gmail.com>
2020-04-24 12:27:32 -0400

    login endpoint

--- sc06/flaskops/Flask-Ops.py
+++ sc06/flaskops/Flask-Ops.py
@@ -1,4 +1,5 @@
 from flask import Flask
+import os
  
 app = Flask(__name__)
  
@@ -6,6 +7,11 @@ app = Flask(__name__)
 @app.route('/')
 def hello_whale():
     return "Hello fellow SRE!"
+
+@app.route('/login', methods=['GET'])
+def logingc():
+    os.system('python autologin-sc.py')
+    return "logged into GC!"
 
 if __name__ == '__main__':
    app.run(host="0.0.0.0", port="80") 
