b9df694ffbd31163f455e684292cdca26ae720e7
reagul <reagul.2007@gmail.com>
2020-05-08 11:11:34 -0400

    V2 automloginsc.sh

--- sc06/autologinsc.sh
+++ sc06/autologinsc.sh
@@ -6,7 +6,7 @@ export SC_USER_NAME="administrator@vsphere.local"
 export SC_USER_PASS="VMware1!"
 
 /usr/bin/expect <(cat << EOF
-spawn kubectl vsphere login --server=kubeapi.corp.local --vsphere-username administrator@vsphere.local --managed-cluster-namespace demo-ns1 --managed-cluster-name tkc3 --insecure-skip-tls-verify
+spawn kubectl vsphere login --server=10.10.20.129 --vsphere-username administrator@vsphere.local --managed-cluster-namespace demo-ns1 --managed-cluster-name tkc3 --insecure-skip-tls-verify
 expect "Password:"
 send "$::env(SC_USER_PASS)\n"
 interact
